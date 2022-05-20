2a698b088c32fa9db0fc4919857010250a0de997
unknown <miramimbembe1991@gmail.com>
2022-05-20 07:12:11 +0530

    first react component css style

---
+++ ecommerce/src/Components/ExpenseItem.js
@@ -0,0 +1,21 @@
+import "./ExpenseItem.css";
+
+function ExpenseItem(){
+    return <div className="expense-item">
+<div>Marsh 28th 2021</div>
+<div className="expense-item__description">
+    <h2>
+
+        Car Insurance 
+    </h2>
+    <div className="expense-item__price">$2000.01</div>
+
+</div>
+
+
+
+
+    </div>
+
+}
+export default ExpenseItem;
