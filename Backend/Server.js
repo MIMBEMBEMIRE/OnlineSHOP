b2e5c3fa12ff46684ba428b6afae11753337db19
unknown <miramimbembe1991@gmail.com>
2022-05-24 06:40:17 +0530

    get request

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,60 +1,52 @@
-require('dotenv/config');
-
+require("dotenv/config");
 const express = require("express");
-const bodyParder = require("body-parser");
+const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 
-
-
 const App = express();
-const api = process.env.API_URL;
-App.use(bodyParder.json());
+App.use(bodyParser.json());
 App.use(morgan("tiny"));
-
 mongoose.connect(process.env.CONNECTION_STRING,{dbName:"Wiki_DB"},function(err){
-        if(!err){
-            console.log("the server is connected succesfully");
-        }else{
-            console.log(err)
-        }
-    });
-    const articlesSchema = {
-        title:String,
-        content:String,
+    if(!err){
+        console.log("the server is running success");
+
+    }else{
+        console.log(err);
     }
-    const Article = mongoose.model("Articles",articlesSchema)
-    App.get("/Articles",function(req,res){
-        Article.find(function(err,fundArticles){
-            if(!err){
-                res.send(fundArticles)
-            }else{
-                res.send(err);
-            }
-        })
+});
 
-    
-    
+const articlesSchema = {
+    title:String,
+    content :String
+};
+const Article = mongoose.model("Articles",articlesSchema)
+
+App.get("/articles",function(req,res){
+
+
+Article.find({},function(err,foundarticle){
+    console.log(foundarticle)
+    res.send(foundarticle)
+
+});
+});
+
+
+
+App.get("/")
+
+
+
+
+App.get("/",function(req,res){
+    res.send("the sever is running ");
 })
-        
-
-    
-        App.post("/Articles",function(req,res){
-            const newarticle = new Article({
-                title:req.body.title,
-                content:req.body.content
-            });
-            newarticle.save();
-            
-        })
-    
-        
-        
-    
 
 
 
 
-App.listen(3000,function(){
-    console.log("the server is running sucess");
+
+App.listen(3000,function(err){
+    console.log("NodeJs server is running succes")
 })
