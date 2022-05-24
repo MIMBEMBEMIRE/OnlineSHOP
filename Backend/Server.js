3ff7d3aa147d859b63258c05f4608c9b297aa1b3
unknown <miramimbembe1991@gmail.com>
2022-05-24 09:37:45 +0530

    how to post a data

--- Backend/Server.js
+++ Backend/Server.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 const App = express();
 App.use(bodyParser.json());
 App.use(morgan("tiny"));
+
 mongoose.connect(process.env.CONNECTION_STRING,{dbName:"Wiki_DB"},function(err){
     if(!err){
         console.log("the server is running success");
@@ -16,33 +17,37 @@ mongoose.connect(process.env.CONNECTION_STRING,{dbName:"Wiki_DB"},function(err){
     }
 });
 
-const articlesSchema = {
-    title:String,
-    content :String
+
+const api = process.env.API_URL;
+
+const productSchema = {
+    name:String,
+    image:String,
+    counInStock:Number
 };
-const Article = mongoose.model("Articles",articlesSchema)
-
-App.get("/articles",function(req,res){
+const Product = mongoose.model("Product",productSchema)
 
 
-Article.find({},function(err,foundarticle){
-    console.log(foundarticle)
-    res.send(foundarticle)
+App.post(`${api}/products`,function(req,res){
+console.log(req.body.name);
+console.log(req.body.image);
+    
+    const product1 = new Product({
+        name:req.body.name,
+        image:req.body.image,
+        counInStock:req.body.counInStock
+    })
+product1.save().then((createdproduc=>{
+    res.status(201).json(createdproduc) 
+})).catch((err)=>{
+    res.status(500).json({
+        error:err,
+        success:false
 
-});
-});
-
-
-
-App.get("/")
-
-
-
-
-App.get("/",function(req,res){
-    res.send("the sever is running ");
+    });
 })
-
+    
+})
 
 
 
